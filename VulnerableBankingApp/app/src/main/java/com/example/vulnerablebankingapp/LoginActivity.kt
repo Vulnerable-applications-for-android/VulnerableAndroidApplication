9724e56be4d8543274f5b13dcc38ee1ae86d8b71
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2020-11-11 12:19:17 +0000

    Added real time database and create account activity.

--- VulnerableBankingApp/app/src/main/java/com/example/vulnerablebankingapp/LoginActivity.kt
+++ VulnerableBankingApp/app/src/main/java/com/example/vulnerablebankingapp/LoginActivity.kt
@@ -1,12 +1,50 @@
 package com.example.vulnerablebankingapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(){
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
         setContentView(R.layout.activity_login)
+    }
+
+    fun buttonLogInOnClick(view: View) {
+        val email = text_field_email.editText?.text.toString()
+        val password = text_field_password.editText?.text.toString()
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Success", "signInWithEmail:success")
+                        val user = mAuth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Error", "signInWithEmail:failure", task.exception)
+                        updateUI(null)
+                    }
+                }
+    }
+
+    fun buttonCreateAccountOnClick(view: View) {
+        val intent = Intent(this, CreateAccountActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun updateUI(user : FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- VulnerableBankingApp/app/src/main/java/com/example/vulnerablebankingapp/ui/home/HomeFragment.kt
+++ VulnerableBankingApp/app/src/main/java/com/example/vulnerablebankingapp/ui/home/HomeFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.vulnerablebankingapp.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -22,7 +23,7 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        val textView: TextView = account_number_text_view
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
