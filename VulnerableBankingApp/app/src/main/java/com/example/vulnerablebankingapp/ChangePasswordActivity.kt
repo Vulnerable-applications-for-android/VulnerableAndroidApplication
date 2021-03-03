d290d97274e29bd1042c1dab910f9b7a905c25d4
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2021-03-03 01:12:55 +0000

    Checks when changing password.

--- VulnerableBankingApp/app/src/main/java/com/example/vulnerablebankingapp/ChangePasswordActivity.kt
+++ VulnerableBankingApp/app/src/main/java/com/example/vulnerablebankingapp/ChangePasswordActivity.kt
@@ -6,8 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.*
 import kotlinx.android.synthetic.main.activity_change_password.*
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_login.text_field_password
@@ -23,7 +23,19 @@ class ChangePasswordActivity : AppCompatActivity() {
 
     fun buttonChangePasswordOnClick(view: View) {
         val password = text_field_password.editText?.text.toString()
-        mAuth.currentUser?.updatePassword(password)
+        if (password != "") {
+            mAuth.currentUser?.updatePassword(password)?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Snackbar.make(view, "Password changed!", Snackbar.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Snackbar.make(view, task.exception?.message.toString(), Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+            Snackbar.make(view, "Password cannot be blank!", Snackbar.LENGTH_SHORT).show()
+        }
         val hideKeyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         hideKeyboard.hideSoftInputFromWindow(LinerLayoutChangePassword.windowToken, 0)
     }
